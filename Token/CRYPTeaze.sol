d49e289bd4875416969b726712f4d039ca8b3bee
Nodezy <80292753+nodezy@users.noreply.github.com>
2022-05-02 12:51:20 -0400

    Update CRYPTeaze.sol

--- Token/CRYPTeaze.sol
+++ Token/CRYPTeaze.sol
@@ -1,47 +1,10 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.9;
 
-/**
- * Standard SafeMath, stripped down to just add/sub/mul/div
- */
-library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-
-        return c;
-    }
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-
-        return c;
-    }
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-    }
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-
-        return c;
-    }
-}
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * BEP20 standard interface.
@@ -59,85 +22,6 @@ interface IBEP20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-
-/**
- * Allows for contract ownership 
- */
-abstract contract Auth {
-    address internal owner;
-
-    constructor(address _owner) {
-        owner = _owner;
-    }
-
-    /**
-     * Function modifier to require caller to be contract deployer
-     */
-    modifier onlyOwner() {
-        require(isOwner(msg.sender), "!Owner"); _;
-    }
-
-    /**
-     * Check if address is owner
-     */
-    function isOwner(address account) public view returns (bool) {
-        return account == owner;
-    }
-
-    /**
-     * Transfer ownership to new address. Caller must be owner. 
-     */
-    function transferOwnership(address payable adr) public onlyOwner {
-        owner = adr;
-        emit OwnershipTransferred(adr);
-    }
-
-    event OwnershipTransferred(address owner);
-}
-
-abstract contract ReentrancyGuard {
-    // Booleans are more expensive than uint256 or any type that takes up a full
-    // word because each write operation emits an extra SLOAD to first read the
-    // slot's contents, replace the bits taken up by the boolean, and then write
-    // back. This is the compiler's defense against contract upgrades and
-    // pointer aliasing, and it cannot be disabled.
-
-    // The values being non-zero value makes deployment a bit more expensive,
-    // but in exchange the refund on every call to nonReentrant will be lower in
-    // amount. Since refunds are capped to a percentage of the total
-    // transaction's gas, it is best to keep them low in cases like this one, to
-    // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
-
-    uint256 private _status;
-
-    constructor() {
-        _status = _NOT_ENTERED;
-    }
-
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
-     * `private` function that does the actual work.
-     */
-    modifier nonReentrant() {
-        // On the first call to nonReentrant, _notEntered will be true
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        // Any calls to nonReentrant after this point will fail
-        _status = _ENTERED;
-
-        _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
-        _status = _NOT_ENTERED;
-    }
 }
 
 interface IOracle {
@@ -655,7 +539,7 @@ contract DividendDistributor is IDividendDistributor {
 
 }
 
-contract CRYPTeaze is IBEP20, Auth, ReentrancyGuard {
+contract CRYPTeaze is IBEP20, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     address WETH;
@@ -741,7 +625,7 @@ contract CRYPTeaze is IBEP20, Auth, ReentrancyGuard {
     
     modifier swapping() { inSwap = true; _; inSwap = false; }
 
-    constructor () Auth(msg.sender) {
+    constructor () {
         
        // router = IDEXRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);  pancake v2
 
@@ -780,19 +664,19 @@ contract CRYPTeaze is IBEP20, Auth, ReentrancyGuard {
 
     }
 
-    receive() external payable { }
+    receive() external payable {}
 
     function totalSupply() external view override returns (uint256) { return _totalSupply; }
     function decimals() external pure override returns (uint8) { return _decimals; }
     function symbol() external pure override returns (string memory) { return _symbol; }
     function name() external pure override returns (string memory) { return _name; }
-    function getOwner() external view override returns (address) { return owner; }
+    function getOwner() external view override returns (address) { return owner(); }
     function balanceOf(address account) public view override returns (uint256) { return _balances[account]; }
     function allowance(address holder, address spender) external view override returns (uint256) { return _allowances[holder][spender]; }
 
 
     function approve(address spender, uint256 amount) public override returns (bool) {
-        if(!isOwner(msg.sender)){require(launchEnabled, "Liquid has not been added yet!");}
+        if(owner() != msg.sender){require(launchEnabled, "Liquid has not been added yet!");}
         _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
