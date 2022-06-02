50996077e57ef9d9ed7e73b5dabddb166d2c1520
Nodezy <80292753+nodezy@users.noreply.github.com>
2022-06-02 10:42:25 -0400

    Create TeazeNFT.sol

---
+++ NFT/TeazeNFT.sol
@@ -0,0 +1,110 @@
+//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+//import "./auth.sol";
+
+interface ITeazePacks {
+    function getPackInfo(uint256 _packid) external view returns (uint256,uint256,uint256,uint256,bool,bool); 
+    function getPackTotalMints(uint256 _packid) external view returns (uint256); 
+}
+
+contract TeazeNFT is Ownable, ERC721URIStorage, ERC721Enumerable, ReentrancyGuard {
+    using Counters for Counters.Counter;
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    Counters.Counter public _tokenIds;
+       
+    mapping(string => uint) private NFTmintedCountURI;  // Get total # minted by URI.
+    mapping(string => bool) private NFTuriExists;  // Get total # minted by URI.
+    mapping(uint256 => uint) private NFTmintedCountID; // Get total # minted by NFTID.
+   
+    address public farmingContract; // Address of the associated farming contract.
+    address public packsContract; // Address of the associated farming contract.
+    uint private minted;
+    
+
+    constructor(address _farming) ERC721("CryptezeNFT", "TeazeNFT") {
+        farmingContract = _farming;
+    }
+
+    receive() external payable {}
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return ERC721URIStorage.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+        return ERC721URIStorage._burn(tokenId);
+    }
+    
+
+    function mint(address _recipient, string memory _uri, uint _packNFTid) public nonReentrant returns (uint256) {
+
+        require(address(farmingContract) != address(0), "Farming contract address is invalid");
+        require(msg.sender == address(farmingContract), "Minting not allowed outside of the farming contract");
+
+        _tokenIds.increment();
+        
+        uint256 newItemId = _tokenIds.current();
+        _mint(_recipient, newItemId);
+        _setTokenURI(newItemId, _uri);
+
+        NFTmintedCountURI[_uri] = NFTmintedCountURI[_uri] + 1;
+
+        NFTmintedCountID[_packNFTid] = NFTmintedCountID[_packNFTid] + 1;
+
+        return newItemId;
+
+    }
+
+    //returns the balance of the erc20 token required for validation
+    function checkBalance(address _token, address _holder) public view returns (uint256) {
+        IERC20 token = IERC20(_token);
+        return token.balanceOf(_holder);
+    }
+    //returns the number of mints for each specific NFT based on URI
+    function mintedCountbyURI(string memory _tokenURI) public view returns (uint256) {
+        return NFTmintedCountURI[_tokenURI];
+    }
+
+    function mintedCountbyID(uint256 _id) public view returns (uint256) {
+        return NFTmintedCountID[_id];
+    }
+
+    function setFarmingContract(address _address) public onlyOwner {
+        farmingContract = _address;
+    }
+
+    
+    // This will allow to rescue ETH sent by mistake directly to the contract
+    function rescueETHFromContract() external onlyOwner {
+        address payable _owner = payable(_msgSender());
+        _owner.transfer(address(this).balance);
+    }
+
+    // Function to allow admin to claim *other* ERC20 tokens sent to this contract (by mistake)
+    function transferERC20Tokens(address _tokenAddr, address _to, uint _amount) public onlyOwner {
+       
+        IERC20(_tokenAddr).transfer(_to, _amount);
+    }
+
+}
+
