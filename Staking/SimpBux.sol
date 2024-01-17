a34a93bd50ebcd34813695eae6887dd9a1872096
Nodezy <80292753+nodezy@users.noreply.github.com>
2024-01-16 22:52:44 -0500

    Update SimpBux.sol

--- Staking/SimpBux.sol
+++ Staking/SimpBux.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title  
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ * Based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.1/contracts/examples/SimpleToken.sol
+ */
+contract SimpBux is ERC20 {
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor(
+        /*string memory name,
+        string memory symbol,
+        uint256 initialSupply*/
+    ) ERC20("SimpBux", "SBX") {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+
+    address owner = payable(address(msg.sender));
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+    _;
+    }
+
+    function rescueETHFromContract() external onlyOwner {
+        address payable _owner = payable(_msgSender());
+        _owner.transfer(address(this).balance);
+    }
+
+    function transferERC20Tokens(address _tokenAddr, address _to, uint _amount) external onlyOwner {
+        require(_tokenAddr != address(this), "Cannot transfer out native token");
+        IERC20(_tokenAddr).transfer(_to, _amount);
+    }
+}
