431b558102f02edb2da8ea37c385782458a11024
Nodezy <80292753+nodezy@users.noreply.github.com>
2024-04-15 22:24:10 -0400

    Update TeazePacks.sol

--- Packs/TeazePacks.sol
+++ Packs/TeazePacks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
